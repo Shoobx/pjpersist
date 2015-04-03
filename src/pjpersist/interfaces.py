2cddddb097d7570a065f8f8d2b48c7d8154d9e71
Antti Haapala <antti.haapala@anttipatterns.com>
2015-04-04 00:20:03 +0300

    final python 3 fixes

--- src/pjpersist/interfaces.py
+++ src/pjpersist/interfaces.py
@@ -23,7 +23,13 @@ import types
 import zope.interface
 import zope.schema
 
-PJ_NATIVE_TYPES = (bool, float, type(None)) + six.integer_types + six.string_types
+PJ_NATIVE_TYPES = (bool, float, type(None)) + six.integer_types + (str,)
+
+if six.PY2:
+    PJ_NATIVE_TYPES += (unicode,)
+else:
+    PJ_NATIVE_TYPES += (bytes,)
+
 REFERENCE_SAFE_TYPES = (
     datetime.datetime, datetime.date, datetime.time, decimal.Decimal)
 
