84dbc21215ffa17343369fad36af803edeb19d74
Noah Durell <ndurell@users.noreply.github.com>
2015-12-31 10:27:15 -0500

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*rnames):
 
 setup(
     name='pjpersist',
-    version='0.8.8',
+    version='0.8.9',
     author="Shoobx Team",
     author_email="dev@shoobx.com",
     url='https://github.com/Shoobx/pjpersist',
