5c304202201f4d4fcd3c86c40bd427f0a2cbbd45
Stephan Richter <stephan.richter@gmail.com>
2017-03-18 11:21:15 -0400

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*rnames):
 
 setup(
     name='pjpersist',
-    version='1.0.0',
+    version='1.0.1.dev0',
     author="Shoobx Team",
     author_email="dev@shoobx.com",
     url='https://github.com/Shoobx/pjpersist',
