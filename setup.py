18ff22bfe0278bab4abc131df8712f3b1c06435a
Stephan Richter <stephan.richter@gmail.com>
2017-10-24 17:43:52 -0400

    Get ready for a release.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*rnames):
 
 setup(
     name='pjpersist',
-    version='1.1.3.dev0',
+    version='1.2.0.dev0',
     author="Shoobx Team",
     author_email="dev@shoobx.com",
     url='https://github.com/Shoobx/pjpersist',
