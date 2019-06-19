fde791c4cd5ffbd99e6a2ce8531ef02784223b96
Adam Groszer <agroszer@gmail.com>
2019-06-19 16:27:03 +0200

    Preparing release 1.7.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='pjpersist',
-    version='1.7.0.dev0',
+    version='1.7.1',
     author="Shoobx Team",
     author_email="dev@shoobx.com",
     url='https://github.com/Shoobx/pjpersist',
